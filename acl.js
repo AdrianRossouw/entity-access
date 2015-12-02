19162d65af97084735129fde1dc71384f0b773e6
Adrian Rossouw <adrian@daemon.co.za>
2015-12-02 23:29:26 +0200

    fix tests

--- acl.js
+++ acl.js
@@ -13,7 +13,7 @@ module.exports = function (db) {
       locks: function(ent, done) {
         var entityId = ent.id;
 
-        acl.locks(ent, function(locks) {
+        acl.locks(ent, function(err, locks) {
           done(null, _processLocks(locks));
         });
 
