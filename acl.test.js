c7f085349d09bc8c1398b50476df464869f0bf08
Adrian Rossouw <adrian@daemon.co.za>
2017-03-16 20:03:18 +0200

    Switch to mocha from lab.

--- acl.test.js
+++ acl.test.js
@@ -1,11 +1,3 @@
-var Lab = require('lab')
-
-var lab = exports.lab = Lab.script()
-var describe = lab.describe
-var it = lab.it
-var before = lab.before;
-var after = lab.after;
-
 var _ = require('lodash');
 var assert = require('assert');
 var ACL = require('./acl');
